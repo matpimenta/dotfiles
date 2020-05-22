607920892843b5afea9e7edde67306e420523784
Mateus Pimenta <1920261+matpimenta@users.noreply.github.com>
2020-05-22 10:22:44 +0100

    Adding p10k source

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ ln -s ~/dotfiles/bash_profile ~/.bash_profile
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/p10k.zsh ~/.p10k.zsh
 
