4e389ae57c806486b01547d23250db2ebeb82c93
Mateus Pimenta <matpimenta@matpimenta>
2020-07-30 13:51:04 +0100

    Enhancing bootstrap script

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -3,12 +3,13 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y zsh cmake tmux vim-gtk
 
+./install.sh
 
 echo ">>> Installing Oh my Zsh"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || true
 
 echo ">>> Install P10K"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k || true
 
 echo ">>> Configuring VIM"
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
