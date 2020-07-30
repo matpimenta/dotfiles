0ea6accd375a5e282fcf2e36e6a56fcb31c9a6be
Mateus Pimenta <matpimenta@matpimenta>
2020-07-30 14:28:29 +0100

    Enhancing bootstrap script

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,12 +1,14 @@
 echo ">>> Installing dependencies"
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y zsh cmake tmux vim-gtk python3-dev build-essential golang-1.14-go
+sudo apt install -y zsh cmake tmux vim-gtk python3-dev build-essential golang-1.14-go nodejs npm autojump
 
 ./install.sh
 
 echo ">>> Installing Oh my Zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || true
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 echo ">>> Install P10K"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k || true
